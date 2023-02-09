a221b86c0e0825f8d05b76806b94a2b775c0970b
AkumuJey <josephowino7@gmail.com>
2023-02-09 15:32:01 +0300

    Updating server data with pinia

--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -28,17 +28,40 @@ export const useTodosStore = defineStore('todosStore', {
             this.todos = data
             this.isLoading = false
         },
-        addTodo(todo) {
+        async addTodo(todo) {
             this.todos.push(todo)
+
+            const res = await fetch('http://localhost:3000/todos', {
+                method: 'POST',
+                body: JSON.stringify(todo),
+                headers: {'Content-Type': 'application/json'}
+            })
+            if (res.error) {
+                console.log(res.error);
+            }
         },
-        deleteTodo(id) {
+        async deleteTodo(id) {
             this.todos = this.todos.filter(todo => {
               return todo.id !== id
             })
+            const res = await fetch('http://localhost:3000/todos/' + id, {
+                method: 'DELETE'
+            })
+            if (res.error) {
+                console.log(res.error);
+            }
           },
-        toggleFav(id) {
+        async toggleFav(id) {
             const todo = this.todos.find(t => t.id === id)
             todo.isFav = !todo.isFav
+            const res = await fetch('http://localhost:3000/todos/' + id, {
+                method: 'PATCH',
+                body: JSON.stringify(todo),
+                headers: {isFav: todo.isFav }
+            })
+            if (res.error) {
+                console.log(res.error);
+            }
         }
     }
 })
